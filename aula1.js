12bd463286be0fdc4abcee988230d8627cbe432f
João Renato Chiquito <joao.iaroczinski@escola.pr.gov.br>
2026-03-09 08:46:59 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,27 @@
+
+// auto descrição em variáveis!
+
+let nome = "João Renato Chiquito Iaroczinski"
+let idade = 15
+let altura = 1.68
+let peso = 60
+let cabelo = "Enrolado e grande pela altura de baixo da orelha"
+let estilo = "estilo meio grunge"
+let personalidade = "Introvertido"
+let manga = "Chainsaw man"
+let anime = "Evangelion"
+let jogo = "War thunder"
+let humor = "tristonho"
+
+console.log("Este é: " + nome)
+console.log("Tem: " + idade)
+console.log("Altura: " + altura)
+console.log("Peso: " + peso)
+console.log("O cabelo dele/dela é: " + cabelo)
+console.log("O estilo é: " + estilo)
+console.log("A personalidade dele/dela é: " + personalidade)
+console.log("O manga favorito é: " + manga)
+console.log("O anime favorito é: " + anime)
+console.log("O jogo favorito dele/dela é: " + jogo)
+console.log("Como está agora: " + humor)
+
