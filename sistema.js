fd9be011540f7229dd34f859c719fc7934e2d72d
João Renato Chiquito <joao.iaroczinski@escola.pr.gov.br>
2026-04-29 09:41:56 -0300

    Add files via upload

--- sistema.js
+++ sistema.js
@@ -1,9 +1,39 @@
 const agentesInfo = require("./agentes_valorant.json")
 console.log(agentesInfo)
 
+function mostrarAgentes(){
 agentesInfo.forEach((agentes)=>{
     console.log(agentes.nome + "---" + "Codinome: " + agentes.codinome + " tem a função de: " + agentes.classe + " faz a função de: " + agentes.funcao + " e tem: " + agentes.idade + " Anos.")
 })
+}
+
+function adicionarItem(itemnovo){
+    agentesInfo.push(itemnovo)
+    
+
+}
+
+//adicione aqui o objeto novo:
+const itemnovo ={
+                nome: "Mateo Armendáriz De la Fuente",
+                codinome: "Gekko",
+                classe: "Iniciador",
+                funcao: "Apoio",
+                idade: "22",
+                }
+
+adicionarItem(itemnovo)
+mostrarAgentes()
+
+
+function converter(){
+     agentesInfo.forEach((agentes)=>{
+        agentes.idade=Number(agentes.idade)
+     }) 
+}
+
+converter()
+console.log(agentesInfo)
 
 
 
