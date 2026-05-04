d0320c64b4dd35c871abef9f19d7c7fb01da3571
João Renato Chiquito <joao.iaroczinski@escola.pr.gov.br>
2026-05-04 08:56:01 -0300

    Add files via upload

--- sistema.js
+++ sistema.js
@@ -1,8 +1,8 @@
 const agentesInfo = require("./agentes_valorant.json")
 console.log(agentesInfo)
 
-function mostrarAgentes(){
-agentesInfo.forEach((agentes)=>{
+function mostrarAgentes(lista){
+lista.forEach((agentes)=>{
     console.log(agentes.nome + "---" + "Codinome: " + agentes.codinome + " tem a função de: " + agentes.classe + " faz a função de: " + agentes.funcao + " e tem: " + agentes.idade + " Anos.")
 })
 }
@@ -22,18 +22,32 @@ const itemnovo ={
                 idade: "22",
                 }
 
-adicionarItem(itemnovo)
-mostrarAgentes()
-
-
 function converter(){
-     agentesInfo.forEach((agentes)=>{
-        agentes.idade=Number(agentes.idade)
-     }) 
+    agentesInfo.forEach((agentes)=>{
+    agentes.idade=Number(agentes.idade)
+  }) 
 }
 
+function filtrar(){
+    var agentesFiltro = agentesInfo.filter((agentes)=> agentes.idade < 25)
+    console.log("***** - Agentes menores de 25 anos: - *****")
+    mostrarAgentes(agentesFiltro)
+}
+
+function aplicarPorcentagem(){
+    var porcentagem = agentesInfo.map((trabalhoRestante) => {
+        return {...agentes, idade: agentes.idade * 0.25}
+    })
+    console.log("***** - Anos restantes de trabalho: - *****")
+}
+ 
+
+adicionarItem(itemnovo)
+mostrarAgentes(agentesInfo)
 converter()
-console.log(agentesInfo)
+filtrar()
+
+
 
 
 
