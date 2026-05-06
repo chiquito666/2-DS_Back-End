08ab544b41402e670e175d4fa68c9449e142a82f
João Renato Chiquito <joao.iaroczinski@escola.pr.gov.br>
2026-05-06 09:44:11 -0300

    Add files via upload

--- recuperação.js
+++ recuperação.js
@@ -1,31 +1,19 @@
 
+/* COPIE E COLE ESTE ENUNCIADO NO VSCODE! */
 
+/* Prova prática de recuperação do nivelamento (AV1) + prova (AV2).
+Pode consultar o seu GitHub ou o do professor: https://github.com/profpatrickoli
+Cópia de outros colegas ou uso de outros sites na internet (Google, IA, etc) = zero na recuperação! 
+A prova pode ser realizada em duplas! Responder cada questão com as informações de cada integrante da dupla, basta criar variáveis com nomes diferentes. Exemplo: aluno1 e aluno2 
+Para enviar a atividade, poste o código no GitHub de algum integrante da dupla e envie o link na atividade do Class. */
 
+/* 1) (1,0 p) Crie variáveis para armazenar seu nome completo, altura, série e turma. Após isso, mostre no terminal uma mensagem personalizada se apresentando. */
 
+/* 2) (1,0 p) Crie uma lista com 3 esportes que você gosta e mostre-os no terminal, usando o laço de repetição que preferir */
 
+/* 3) (1,5 p) Crie uma condição que verifica se você é maior que o professor Patrick, que possui 1.73 de altura */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* 4) (1,5 p) Crie um laço de repetição que conta de 0 até -10 */
 
 
 
